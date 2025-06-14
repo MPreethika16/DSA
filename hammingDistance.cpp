997dd4fdfd3ea31bc119bd97acd19b8bf7c932bf
MPreethika <manepallypreethika@gmail.com>
2025-06-14 21:44:13 +0530

    completed

---
+++ hammingDistance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ans=x^y;
+        int c=0;
+        while(ans){
+            ans&=ans-1;
+            c+=1;
+        }
+        return c;
+    }
+};
