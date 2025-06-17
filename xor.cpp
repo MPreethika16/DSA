51c36b17e5cc403721f00b5993394e4b5ef67c14
MPreethika <manepallypreethika@gmail.com>
2025-06-17 23:35:27 +0530

    updated

--- xor.cpp
+++ xor.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        int ans=0;
+        int answer=0;
         vector<int>res(n);
         for(int i=0;i<n;i++){
             res[i]=start+2*i;
-            ans^=res[i];
+            answer^=res[i];
         }
-        return ans;
+        return answer;
     }
-};
