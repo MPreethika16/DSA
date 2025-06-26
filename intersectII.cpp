efa8449734a9bd71f8ecadb075eba4d2fdd8a7cb
MPreethika <manepallypreethika@gmail.com>
2025-06-26 22:45:07 +0530

    map

---
+++ intersectII.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>res;
+        int n=nums1.size();
+        int m=nums2.size();
+        unordered_map<int,int>mp;
+        if(n>m){
+            return intersect(nums2,nums1);
+        }
+        for(int num:nums1){
+            mp[num]++;
+        }
+        for(int num:nums2){
+            if(mp.find(num)!=mp.end()&&mp[num]>0){
+                res.push_back(num);
+                mp[num]--;
+            }
+        }
+        return res;
+    }
+};
