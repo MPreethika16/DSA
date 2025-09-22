d0b6b3a4233262b0afc9532477dc06964d1a367d
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-09-22 19:39:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (99.02%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+       vector<vector<int>>dp;
+    //   dp[numRows-1][0]=1;
+       for(int i=0;i<numRows;i++){
+            vector<int>rows(i+1,1);
+        for(int j=1;j<i;j++){
+            rows[j]=dp[i-1][j-1]+dp[i-1][j];
+        }
+        dp.push_back(rows);
+       }
+       return dp;
+    }
+};
