0d0cbb603b0484012fc42eafddf77c9012452e5a
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-11-21 00:55:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (46.08%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int uniquePaths(int m, int n) {
+        if(m==1&&n==1){
+            return 1;
+        }
+        dp.assign(m,vector<int>(n,1));
+        dp[0][0]=0;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+               // if(i<0||j<0||i>m-1||j>n-1) return dp[];
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+};
+
+
+
