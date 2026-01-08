ef6b679f8164643022a6f701afe8b90255852baa
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2026-01-08 13:39:13 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (49.85%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>>vp;
+        int rowcol0=1;
 
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
                 if(matrix[i][j]==0){
-                    vp.push_back({i,j});
+                    matrix[i][0]=0;
+                    if(j!=0)
+                        matrix[0][j]=0;
+                    else
+                        rowcol0=0;
                 }
             }
         }
 
-        for(auto p:vp){
-            int k=p.first;
-            int l=p.second;
-            for(int i=0;i<matrix[0].size();i++){
-                matrix[k][i]=0;
-            }
-            for(int j=0;j<matrix.size();j++){
-                matrix[j][l]=0;
+        for(int i=matrix.size()-1;i>=1;i--){
+            for(int j=matrix[0].size()-1;j>=1;j--){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0||matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
             }
         }
-        
+        if(matrix[0][0]==0) for(int j=0;j<matrix[0].size();j++) matrix[0][j]=0;
+        if(rowcol0==0) for(int i=0;i<matrix.size();i++) matrix[i][0]=0;
     }
 };
